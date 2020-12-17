58798028eab0c7d3f938452008c017acff92d4ba
kayode osunjaye <kayodesnj@gmail.com>
2020-12-16 22:20:23 -0600

    first comit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kayPublicLib.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
